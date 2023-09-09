bd390cbe830b2b74fc2c614ca688f80a9e41c750
Kiran Raj <kiranrajb5882@gamil.com>
2023-09-09 19:44:11 +0530

    a variable is added

--- jdk/add.java
+++ jdk/add.java
@@ -3,5 +3,7 @@ package Git.jdk;
 public class add {
     public static void main(String[] args) {
         System.out.println("add two numbers ");
+        int a =10;
+        System.out.println("the variable a = "+a);
     }
 }
